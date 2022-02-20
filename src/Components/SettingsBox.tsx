3d94d21fe1f178b226fbfc530a641891075b3f20
ElenaHrudavenka <kisseli@mail.ru>
2022-02-20 22:13:03 +0300

    Update SettingsBox.tsx

--- src/Components/SettingsBox.tsx
+++ src/Components/SettingsBox.tsx
@@ -1,6 +1,7 @@
 import React, {ChangeEvent, useState} from 'react';
-import { SettingsType } from '../App';
+import {SettingsType} from '../App';
 import Button from './Button';
+import Input from './Input';
 
 type SettingsPropsType = {
     settings: SettingsType
@@ -8,21 +9,39 @@ type SettingsPropsType = {
 }
 
 const SettingsBox = (props: SettingsPropsType) => {
-    const [localSettings, setLocalSettings] = useState<SettingsType>(props.settings)
+    const [localSettings, setLocalSettings] = useState<SettingsType>({
+        setMin: props.settings.setMin,
+        setMax: props.settings.setMax,
+        setStep: props.settings.setStep,
+    })
     const onClickHandler = () => {
 
     }
-    const onChangeHandler = (e:ChangeEvent<HTMLInputElement>) => {
 
+    const onChangeHandlerMin = (eValue: number) => {
+        eValue<localSettings.setMax && setLocalSettings({...localSettings, setMin: eValue})
     }
+    const onChangeHandlerMax = (eValue:number) => {
+        eValue>localSettings.setMin ? setLocalSettings({...localSettings, setMax:eValue}) : alert("Errror")
+    }
+    const onClickHandlerStep = (eValue: number) => {
+        eValue>=0 && setLocalSettings({...localSettings, setStep:eValue})
+    }
+    const onFocusSettings = () => {
+        
+    }
+
     return (
         <div>
             <h3>Settings</h3>
-            <input type="number" value={props.settings.setMin} onChange={onChangeHandler}/>
-            <input type="number" value={props.settings.setMax}/>
-            <input value={props.settings.setStep}/>
+            <span>Set maximum value</span>
+            <Input type="number" value={localSettings.setMax} onChange={onChangeHandlerMax} step={localSettings.setStep}/>
+            <span> Set minimum value</span>
+            <Input type="number" value={localSettings.setMin} onChange={onChangeHandlerMin} step={localSettings.setStep}/>
+            <span>Set step</span>
+            <Input type = "number" value={localSettings.setStep} onChange={onClickHandlerStep}/>
             {/*прикрутить файл настройки*/}
-            <Button name="SET" disabled={props.disabled} callBack={onClickHandler}/>
+            <Button name="SET" disabled={props.disabled} callBack={onClickHandler}/> <span onFocus={onFocusSettings}>?</span>
         </div>
     );
 };
