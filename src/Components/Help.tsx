30719f4e411fc6126d2ebf2b5c787d836b28bb7e
ElenaHrudavenka <kisseli@mail.ru>
2022-02-21 16:21:54 +0300

    Create Help.tsx

---
+++ src/Components/Help.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export const Help = () => {
+    return (
+        <div>
+            <h3>Help</h3>
+            <ul>
+                <li>The minimum value cannot be greater than the maximum value</li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    );
+};
+//Минимальное значение не может быть больше максимального значения
+//
