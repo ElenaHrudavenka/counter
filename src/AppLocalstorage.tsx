c98529fb4a4274b7cce8ae4661e72c2cb216314f
ElenaHrudavenka <kisseli@mail.ru>
2022-03-23 18:31:15 +0300

    Update AppLocalstorage.tsx

--- src/AppLocalstorage.tsx
+++ src/AppLocalstorage.tsx
@@ -1,8 +1,17 @@
 import React, {useState, useEffect}  from 'react';
+import {useDispatch, useSelector } from 'react-redux';
+import { incCounterValueAC } from './bll/counter-reducer';
+import { AppStateType } from './bll/store';
 
 
 const AppLocalstorage = () => {
-    const [value, setValue] = useState<number>(0)
+    const value = useSelector< AppStateType, number >( state => state.counter.value)
+    const dispatch = useDispatch()
+
+    const onClickHandler = () => {
+        dispatch(incCounterValueAC())
+    }
+/*    const [value, setValue] = useState<number>(0)
 
     const getToLocalStorageHandler = () => {
         let valueAsString = localStorage.getItem('counterValue')
@@ -15,13 +24,13 @@ const AppLocalstorage = () => {
 
     const setToLocalStorageHandler = () => {
         localStorage.setItem('counterValue', JSON.stringify(value))
-/*        localStorage.setItem('counterValue + 1', JSON.stringify(value + 1))*/
+/!*        localStorage.setItem('counterValue + 1', JSON.stringify(value + 1))*!/
     }
     useEffect(setToLocalStorageHandler, [value])
 
     const onClickHandler = () => {
         setValue(value+1)
-    }
+    }*/
 
 /*    const clearLocalStorageHandler = () => {
         localStorage.clear()
@@ -34,7 +43,6 @@ const AppLocalstorage = () => {
         <div>
             <h1>{value}</h1>
             <button onClick={onClickHandler}>inc</button>
-
         </div>
     );
 };
